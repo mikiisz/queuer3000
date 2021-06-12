010e8b79bd03a1b698d6ef1bf7cba85b897bfd19
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2021-06-12 13:18:41 +0200

    Update scripts

--- python/consumer.py
+++ python/consumer.py
@@ -1,25 +1,26 @@
 import queue_wrapper
 import message_wrapper
 import time
-import time
+import math
 
 queue = queue_wrapper.get_queue("My-test-queue")
 
-start_time = time.time()
-
 amount = 0
-
 times = []
+h = 10
+f = None
 
-for i in range(10000):
-    messages = message_wrapper.receive_messages(queue, 1, wait_time = 20)
+for i in range(math.ceil(10000/h)):
+    messages = message_wrapper.receive_messages(queue, h, wait_time = 20) 
     for m in messages:
         times.append(float(time.time()) - float(m.body))
     if len(messages) > 0:
+        if f is None:
+            f = float(messages[0].body)
         message_wrapper.delete_messages(queue, messages)
 
     amount = amount + len(messages)
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - f))
 print(sum(times)/amount)
 print("Received %s messages" % amount)
--- python/producer.py
+++ python/producer.py
@@ -5,8 +5,3 @@ import time
 queue = queue_wrapper.get_queue("My-test-queue")
 for i in range(10000):
     message_wrapper.send_message(queue, str(time.time()) )
-~                                                                                                   
-~                                                                                                   
-~                                                                                                   
-~                                                                                                   
-~                                                                   
